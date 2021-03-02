8521606bf1face344e34537b8f15f48cc35277bf
JonasKristiansen <55428619+JonasArvadKristiansen@users.noreply.github.com>
2021-03-02 13:50:17 +0100

    Add files via upload

---
+++ ADIntegration/ADIntegration/ADIntegration/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace ADIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchResultCollection results;
+            string ip = "LDAP://192.168.132.10";
+            string user = "Administrator";
+            string password = "Password1";
+            DirectoryEntry de = new DirectoryEntry(ip, user, password);
+            DirectorySearcher ds = new DirectorySearcher(de);
+            ds.Filter = "(&(objectCategory=User)(objectClass=person))";
+            results = ds.FindAll();
+            foreach(SearchResult res in results) 
+            {
+                Console.WriteLine(res.Properties["name"][0].ToString());
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
