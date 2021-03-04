e7d61fad8ab7ce5475e1e0d9316197ca7c607428
kevin4845 <72731926+kevin4845@users.noreply.github.com>
2021-03-04 13:41:28 +0100

    Search Engine

--- ADIntegration/ADIntegration/ADIntegration/Program.cs
+++ ADIntegration/ADIntegration/ADIntegration/Program.cs
@@ -7,34 +7,83 @@ namespace ADIntegration
     {
         static void Main(string[] args)
         {
+            App();
+        }
+
+        static void App()
+        {
+            Console.Clear();
             //Name, Mail, Mobile, Telephone, Address, Postal
             SearchResultCollection results;
             string ip = "LDAP://192.168.132.10";
-            string user = "Administrator";
-            string password = "Password1";
+
+            Console.WriteLine("Enter Username: ");
+            string user = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Enter Password: ");
+            string password = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("How do you want to lookup this person? Mail/Name/Mobile: ");
+            string searchOption = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Value: ");
+            string input = Console.ReadLine();
+
+            Console.Clear();
+
             string[] options = { "Name", "Mail", "Mobile", "TelephoneNumber", "StreetAddress", "PostalCode" };
 
-            DirectoryEntry de = new DirectoryEntry(ip, user, password);
-            DirectorySearcher ds = new DirectorySearcher(de);
-            ds.Filter = "(&(objectCategory=User)(objectClass=person))";
-            results = ds.FindAll();
-
-            foreach (SearchResult res in results)
+            try
             {
-                foreach (string option in options)
+                DirectoryEntry de = new DirectoryEntry(ip, user, password);
+                DirectorySearcher ds = new DirectorySearcher(de);
+
+                switch (searchOption.ToLower())
                 {
-                    try{
-                        Console.WriteLine($"{option}: {res.Properties[option][0].ToString()}");
-                    }
-                    catch{
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"{option} is missing");
-                        Console.ResetColor();
-                    }
+                    case "mail":
+                        ds.Filter = "(&(objectCategory=User)(objectClass=person)(mail=" + input + "*))";
+                        break;
+                    case "name":
+                        ds.Filter = "(&(objectCategory=User)(objectClass=person)(name=" + input + "*))";
+                        break;
+                    case "mobile":
+                        ds.Filter = "(&(objectCategory=User)(objectClass=person)(mobile=" + input + "*))";
+                        break;
+
                 }
-                Console.WriteLine();
+
+                results = ds.FindAll();
+
+                foreach (SearchResult res in results)
+                {
+                    foreach (string option in options)
+                    {
+                        try
+                        {
+                            Console.WriteLine($"{option}: {res.Properties[option][0].ToString()}");
+                        }
+                        catch
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"{option} is missing");
+                            Console.ResetColor();
+                        }
+                    }
+                    Console.WriteLine();
+                }
             }
-            Console.ReadKey();
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Something went wrong :(");
+                Console.WriteLine("Press any key to try again");
+                while (Console.ReadKey().Key != ConsoleKey.Enter)
+                {
+                    App();
+                }
+            }
 
         }
     }
