505471faef2ba796b7e5ad4f8d3aa7ac1023d21b
Jesper Kjærgaard <jekj.skp@edu.mercantec.dk>
2021-03-04 09:45:28 +0100

    Update

--- ADIntegration/ADIntegration/ADIntegration/Program.cs
+++ ADIntegration/ADIntegration/ADIntegration/Program.cs
@@ -7,17 +7,32 @@ namespace ADIntegration
     {
         static void Main(string[] args)
         {
+            //Name, Mail, Mobile, Telephone, Address, Postal
             SearchResultCollection results;
             string ip = "LDAP://192.168.132.10";
             string user = "Administrator";
             string password = "Password1";
+            string[] options = { "Name", "Mail", "Mobile", "TelephoneNumber", "StreetAddress", "PostalCode" };
+
             DirectoryEntry de = new DirectoryEntry(ip, user, password);
             DirectorySearcher ds = new DirectorySearcher(de);
             ds.Filter = "(&(objectCategory=User)(objectClass=person))";
             results = ds.FindAll();
-            foreach(SearchResult res in results) 
+
+            foreach (SearchResult res in results)
             {
-                Console.WriteLine(res.Properties["name"][0].ToString());
+                foreach (string option in options)
+                {
+                    try{
+                        Console.WriteLine($"{option}: {res.Properties[option][0].ToString()}");
+                    }
+                    catch{
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{option} is missing");
+                        Console.ResetColor();
+                    }
+                }
+                Console.WriteLine();
             }
             Console.ReadKey();
 
