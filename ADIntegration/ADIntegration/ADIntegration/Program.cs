a8c820b9e299412d844aeb67bddd5253d95ebfb2
Jonas <jonasarvad2@gmail.com>
2021-03-05 10:35:05 +0100

    done

--- ADIntegration/ADIntegration/ADIntegration/Program.cs
+++ ADIntegration/ADIntegration/ADIntegration/Program.cs
@@ -1,120 +1,95 @@
 ﻿using System;
 using System.DirectoryServices;
-
 namespace ADIntegration
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             App();
         }
-
-        public static void App()
+        static void App()
         {
-            Console.Clear();
-            SearchResultCollection results;
-            string ip = "LDAP://192.168.132.10";
-            string user = "Administrator";
-            string pass = "Password1";
-            DirectoryEntry de = new DirectoryEntry(ip, user, pass);
-            DirectorySearcher ds = new DirectorySearcher(de);
-            ds.Filter = "(&(objectCategory=User)(objectClass=person))";
-            results = ds.FindAll();
-            foreach (SearchResult res in results)
+            try
             {
-                Console.WriteLine(res.Properties["name"][0].ToString());
+                Console.Clear();
+                SearchResultCollection results;
 
-            }
-            Console.WriteLine();
+                string ip = "LDAP://192.168.132.10";
+                Console.WriteLine("Enter Username: ");
 
-            Console.WriteLine("Username: ");
-            string username = Console.ReadLine();
-            Console.Clear();
+                string user = Console.ReadLine();
+                Console.Clear();
 
-            foreach (SearchResult res in results)
-            {
-                if (res.Properties["name"][0].ToString() == username)
+                Console.WriteLine("Enter Password: ");
+                string password = Console.ReadLine();
+
+                DirectoryEntry de = new DirectoryEntry(ip, user, password);
+                DirectorySearcher ds = new DirectorySearcher(de);
+
+                results = ds.FindAll();
+
+                Console.Clear();
+                Console.WriteLine("How do you want to lookup this person? Mail/Name/Mobile: ");
+
+                string searchOption = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Value: ");
+                string input = Console.ReadLine();
+
+                Console.Clear();
+                string[] options = { "Name", "Mail", "Mobile", "TelephoneNumber", "StreetAddress", "PostalCode" };
+
+                switch (searchOption.ToLower())
                 {
-                    try
+                    case "mail":
+                        ds.Filter = "(&(objectCategory=User)(objectClass=person)(mail=" + input + "*))";
+                        break;
+                    case "name":
+                        ds.Filter = "(&(objectCategory=User)(objectClass=person)(name=" + input + "*))";
+                        break;
+                    case "mobile":
+                        ds.Filter = "(&(objectCategory=User)(objectClass=person)(mobile=" + input + "*))";
+                        break;
+                }
+                results = ds.FindAll();
+
+                foreach (SearchResult res in results)
+                {
+                    foreach (string option in options)
                     {
-                        Console.WriteLine("Name: " + res.Properties["name"][0].ToString());
-                    }
-                    catch (Exception)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Name: None");
-                        Console.ResetColor();
+                        try
+                        {
+                            Console.WriteLine($"{option}: {res.Properties[option][0].ToString()}");
+                        }
+                        catch
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"{option} is missing");
+                            Console.ResetColor();
+                        }
                     }
 
-                    try
-                    {
-                        Console.WriteLine("Mail: " + res.Properties["mail"][0].ToString());
-                    }
-                    catch (Exception)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Mail: None");
-                        Console.ResetColor();
-                    }
-
-                    try
-                    {
-                        Console.WriteLine("Mobile: " + res.Properties["mobile"][0].ToString());
-                    }
-                    catch (Exception)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Mobile: None");
-                        Console.ResetColor();
-                    }
-
-                    try
-                    {
-                        Console.WriteLine("Telephone: " + res.Properties["telephoneNumber"][0].ToString());
-                    }
-                    catch (Exception)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Telephone: None");
-                        Console.ResetColor();
-                    }
-
-                    try
-                    {
-                        Console.WriteLine("Address: " + res.Properties["streetAddress"][0].ToString());
-                    }
-                    catch (Exception)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Address: None");
-                        Console.ResetColor();
-                    }
-
-                    try
-                    {
-                        Console.WriteLine("Postal: " + res.Properties["postalCode"][0].ToString());
-                    }
-                    catch (Exception)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Postal: None");
-                        Console.ResetColor();
-                    }
-
-
+                    Console.WriteLine();
                 }
 
+                Console.WriteLine("Vil du prøve igen");
+                string forsætOgExit = Console.ReadLine();
+                if (forsætOgExit.ToLower() == "forsæt" || forsætOgExit.ToLower() == "ja")
+                {
+                    App();
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
             }
-
-            Console.WriteLine("");
-            Console.WriteLine("Press any key to go back");
-            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            catch
             {
+                Console.WriteLine("Sorry");
                 App();
             }
-
         }
-
     }
 }
